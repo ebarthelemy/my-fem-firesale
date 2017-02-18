1e200f606424b6bb3c5ba7c60e1a207576a657de
Edouard Barthelemy <edouardbarthelemy@Edouards-iMac.local>
2017-02-17 19:54:08 -0500

    Add project.

--- app/main.js
+++ app/main.js
@@ -1,17 +1,117 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron')
+const fs = require('fs')
 
-let mainWindow = null;
+let mainWindow = null
+const windows = new Set()
+const fileWatchers = new Map()
+
+const createWindow = exports.createWindow = (file) => {
+  let newWindow = new BrowserWindow({ show: false })
+  windows.add(newWindow)
+
+  newWindow.loadURL(`file://${__dirname}/index.html`)
+
+  newWindow.once('ready-to-show', () => {
+    if (file) openFile(newWindow, file)
+    newWindow.show()
+  })
+
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      event.preventDefault()
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: "Quit with unsaved changes?!",
+        message: "Your changes will be lost if you do not save first.",
+        buttons: [
+          "Quit anyway",
+          "Cancel"
+        ],
+        defaultId: 1,
+        cancelId: 1
+      })
+
+      if (result === 0) newWindow.destroy()
+    }
+  })
+
+  newWindow.on('closed', () => {
+    windows.delete(newWindow)
+    stopWatchingFile(newWindow)
+    newWindow = null
+  })
+}
+
+const getFileFromUserSelection = exports.getFileFromUserSelection = (targetWindow) => {
+  const files = dialog.showOpenDialog(targetWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: "Text Files", extensions: ['txt', 'text'] },
+      { name: "Markdown Files", extensions: ['md', 'markdown'] }
+    ]
+  })
+
+  if (!files) return
+
+  return files[0]
+}
+
+const openFile = exports.openFile = (targetWindow, filePath) => {
+  const file = filePath || getFileFromUserSelection(targetWindow)
+  const content = fs.readFileSync(file).toString()
+
+  app.addRecentDocument(file)
+  startWatchingFile(targetWindow, file)
+
+  targetWindow.webContents.send('file-opened', file, content)
+  // targetWindow.setTitle(`${file} - Fire Sale`)
+  targetWindow.setRepresentedFilename(file)
+}
+
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+
+  if (!file) {
+    file = dialog.showSaveDialog(targetWindow, {
+      title: "Save Markdown",
+      defaultPath: app.getPath('documents'),
+      filters: [
+        { name: "Markdown Files", extensions: ['md', 'markdown'] }
+      ]
+    })
+  }
+
+  if (!file) return;
+
+  fs.writeFileSync(file, content)
+  targetWindow.webContents.send('file-opened', file, content)
+}
+
+const startWatchingFile = (targetWindow, file) => {
+  stopWatchingFile(targetWindow)
+
+  const watcher = fs.watch(file, (event) => {
+    if (event === 'change') {
+      const content = fs.readFileSync(file).toString()
+      targetWindow.webContents.send('file-changed', file, content)
+    }
+  })
+
+  fileWatchers.set(targetWindow, watcher)
+}
+
+const stopWatchingFile = (targetWindow) => {
+  if (fileWatchers.has(targetWindow)) {
+    fileWatchers.get(targetWindow).close()
+    openFile.delete(targetWindow)
+  }
+}
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
+  createWindow()
+})
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-});
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, filePath) => {
+    createWindow(filePath)
+  })
+})
--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,79 @@
-const marked = require('marked');
+const marked = require('marked')
+const { remote, ipcRenderer, shell } = require('electron')
+const mainProcess = remote.require('./main')
+const currentWindow = remote.getCurrentWindow()
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
+const markdownView = document.querySelector('#markdown')
+const htmlView = document.querySelector('#html')
+const newFileButton = document.querySelector('#new-file')
+const openFileButton = document.querySelector('#open-file')
+const saveMarkdownButton = document.querySelector('#save-markdown')
+const revertButton = document.querySelector('#revert')
+const saveHtmlButton = document.querySelector('#save-html')
+const showFileButton = document.querySelector('#show-file')
+const openInDefaultButton = document.querySelector('#open-in-default')
+
+let filePath = null
+let originalContent = ''
+
+const renderMarkdownToHtml = (markdown) => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true })
+}
+
+const updateEditedState = (isEdited) => {
+  currentWindow.setDocumentEdited(isEdited)
+
+  saveMarkdownButton.disabled = !isEdited
+  revertButton.disabled = !isEdited
+
+  let title = "Fire Sale"
+  if (filePath) title = `${filePath} - ${title}`
+  if (isEdited) title = `${title} (Edited)`
+  currentWindow.setTitle(title)
+}
+
+markdownView.addEventListener('keyup', (event) => {
+  const currentContent = event.target.value
+  renderMarkdownToHtml(currentContent)
+  updateEditedState(currentContent !== originalContent)
+})
+
+newFileButton.addEventListener('click', () => {
+  mainProcess.createWindow()
+})
+
+openFileButton.addEventListener('click', () => {
+  mainProcess.openFile(currentWindow)
+})
+
+saveMarkdownButton.addEventListener('click', () => {
+  mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value)
+})
+
+showFileButton.addEventListener('click', () => {
+  shell.showItemInFolder(filePath)
+})
+
+openFileButton.addEventListener('click', () => {
+  shell.openItem(filePath)
+})
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file
+  originalContent = content
+
+  markdownView.value = content
+  renderMarkdownToHtml(content)
+
+  updateEditedState(false)
+})
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+  filePath = filePath
+  originalContent = content
+
+  markdownView.value = content
+  renderMarkdownToHtml(content)
+
+  updateEditedState(false)
+})
